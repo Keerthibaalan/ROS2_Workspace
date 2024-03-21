754c4aae24f8ece3cca7ba356cba0bdc1f21f5a8
Keerthibaalan <tkeerthibaalan@gmail.com>
2024-03-21 20:28:09 +0100

    arduinobot_ws

---
+++ arduinobot_ws/build/arduinobot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/keerthibaalan/ROS2_Workspace/arduinobot_ws/src/arduinobot_bringup
+# Build directory: /home/keerthibaalan/ROS2_Workspace/arduinobot_ws/build/arduinobot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ arduinobot_ws/build/arduinobot_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/keerthibaalan/ROS2_Workspace/arduinobot_ws/src/arduinobot_msgs
+# Build directory: /home/keerthibaalan/ROS2_Workspace/arduinobot_ws/build/arduinobot_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("arduinobot_msgs__py")
