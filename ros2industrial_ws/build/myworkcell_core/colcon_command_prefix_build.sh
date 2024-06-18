68ebd4f7eac7042e8f70f672062beded83e840af
Keerthibaalan <tkeerthibaalan@gmail.com>
2024-06-18 21:50:36 +0200

    18/06/2024 ros_industrial

--- ros2industrial_ws/build/myworkcell_core/colcon_command_prefix_build.sh
+++ ros2industrial_ws/build/myworkcell_core/colcon_command_prefix_build.sh
@@ -1,1 +1,2 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/keerthibaalan/ROS2_Workspace/ros2industrial_ws/install/fake_ar_publisher/share/fake_ar_publisher/package.sh"
